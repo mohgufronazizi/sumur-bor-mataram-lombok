3ba367819b324c8c231ba3800e0cf0775c8fee0c
mohgufronazizi <mohgufronazizi@gmail.com>
2021-12-03 22:38:35 +0700

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+		
+		// add padding top to show content behind navbar
+		navbar_height = document.querySelector('.navbar').offsetHeight;
+		document.body.style.paddingTop = navbar_height + 'px';
+
+	}); 
+	// DOMContentLoaded  end
